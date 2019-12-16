e479f9c96d64e55485f61b3487d44006358bc005
OhJayDubya <owen.wright@leighton.com>
2019-12-16 23:55:58 +0000

    God commit of a whole new UI and functionality for the chat

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
-import express from 'express';
-import http from 'http';
-import socketio from 'socket.io';
+import express from "express";
+import http from "http";
+import socketio from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
 const io = new socketio(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+// Array for storing currently active users
+let users = [];
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-io.on('connection', (socket) => {
-    socket.broadcast.emit('connection', 'A user has connected!');
 
-    socket.on('disconnect', () => {
-        io.emit('disconnect', 'A user has disconnected!');
-    })
+io.on("connection", socket => {
 
-    socket.on('chat message', msg => {
-        io.emit('chat message', msg);
-    })
+    // Fetches all currently active users in the chatroom
+  socket.emit("get users", users);
+
+  // When a user disconnects it updates the user list and sends broadcast
+  socket.on("disconnect", () => {
+    if (socket.username !== undefined) {
+      users = users.filter(el => el.id !== socket.id);
+      io.emit(
+        "user disconnect",
+        `${socket.username} has left the chat!`,
+        socket.id
+      );
+    }
+  });
+
+  // Deals with the sending of user messsages
+  socket.on("message", message => {
+    const username = socket.username;
+    socket.broadcast.emit("message", message, username);
+  });
+
+  // When a user connects and creates a username they are added
+  // to the users array and the chatroom it notified
+  socket.on("user connect", username => {
+    socket.username = username;
+
+    const user = { id: socket.id, username: username };
+
+    users.push(user);
+    io.emit("user connect", `${username} has joined the chat!`, user);
+  });
 });
 
 server.listen(3000, () => {
-    console.log('Listening on *:3000');
+  console.log("Listening on *:3000");
 });
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,32 +1,115 @@
 const socket = io();
+let username = undefined;
 
-const form = document.querySelector('form');
+// Used to create message bubble on the UI when triggered
+const generateMessage = (message, username, origin) => {
 
-const generateMessage = msg => {
-    const node = document.createElement('li');
-    const textNode = document.createTextNode(msg);
-    node.appendChild(textNode);
-    document.getElementById('messages').appendChild(node);
-}
+    // Sets time when message was sent
+  const time = new Date().toLocaleString("en-uk", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true
+  });
 
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    socket.emit('chat message',  document.getElementById('message').value)
-    document.getElementById('message').value = '';
-})
+  // Variables for element creation
+  const parent = document.getElementById("chat");
+  const node = document.createElement("li");
 
-socket.on('chat message', msg => {
-    console.log('Chat Message');
-    generateMessage(msg);
-})
+  // Renders the chat message and automatically scrolls the chat
+  node.setAttribute("class", origin);
+  node.innerHTML = `
+    <div class="entete">
+      <span class="status green"></span>
+      <h2>${username}</h2>
+      <h3>${time}</h3>
+    </div>
+    <div class="message">
+        ${message}
+    </div>
+    `;
+  parent.appendChild(node);
+  parent.scrollTop = parent.scrollHeight;
+};
 
-socket.on('connection', msg => {
-    console.log('User Connected');
-    generateMessage(msg);
-})
+// Used for broadcasting messages to all users
+const generateBroadcast = message => {
+  const parent = document.getElementById("chat");
+  const node = document.createElement("li");
+  node.innerHTML = `<div class="broadcast">${message}</div>`;
+  parent.appendChild(node);
+  parent.scrollTop = parent.scrollHeight;
+};
 
-socket.on('disconnect', msg => {
-    console.log('User Disconnected');
-    generateMessage(msg);
+// Takes the message by the user and emits it to other users
+const sendMessage = event => {
+  event.preventDefault();
+  const message = document.getElementById("message").value;
+
+  if (message) {
+    generateMessage(message, username, "sent");
+    document.getElementById("message").value = "";
+    socket.emit("message", message);
+  }
+};
+
+// Generates the user list of the side of the chat
+const generateUser = (id, username) => {
+  const node = document.createElement("li");
+  node.setAttribute("id", id);
+  node.innerHTML = `
+      <img src="https://i.pravatar.cc/300" alt="avatar" />
+      <div>
+        <h2>${username}</h2>
+        <h3>
+          <span class="status green"></span>
+          online
+        </h3>
+      </div>
+      `;
+  document.getElementById("users").appendChild(node);
+};
+
+// Removes user from list when disconnected
+const removeUser = id => {
+  const node = document.getElementById(id);
+  document.getElementById("users").removeChild(node);
+};
+
+// Sets the users chosen username when they first connect
+const setUsername = event => {
+  event.preventDefault();
+
+  username = document.getElementById("username").value;
+
+  if (username) {
+    document.getElementById("chat-form").style.display = "block";
+    document.getElementById("user-form").style.display = "none";
+    socket.emit("user connect", username);
+  }
+};
+
+// Socket Events
+
+// Triggers when a new message is sent
+socket.on("message", (message, username) => {
+  generateMessage(message, username, "recieved");
 });
 
+// Triggers when a new user connects to the chatroom
+socket.on("user connect", (message, user) => {
+  generateBroadcast(message);
+  generateUser(user.id, user.username);
+});
+
+// Triggers when a user disconnects from that chatroom
+socket.on("user disconnect", (message, id) => {
+  generateBroadcast(message);
+  removeUser(id);
+});
+
+// Gets currently active users in the chat room
+socket.on("get users", users => {
+  users.forEach(user => {
+    generateUser(user.id, user.username);
+  });
+});
